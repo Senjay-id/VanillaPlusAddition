1df56b935cdf7684c9f7ae442a4ec517059f8871
Senjay-id <110238760+Senjay-id@users.noreply.github.com>
2023-11-28 11:56:24 +0700

    Base file

---
+++ Patches/SenjayVanillaPlusAddition_SkipIntro.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace SkipInitSelection
+{
+    [HarmonyPatch(typeof(PreInitSceneScript), "Awake")]
+    internal class SkipInitSelectionClass
+    {
+        [HarmonyPrefix]
+        static void SkipInitSelection(ref bool ___choseLaunchOption)
+        {
+            ___choseLaunchOption = true;
+            SceneManager.LoadScene("InitScene");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SkipInitSelection;
+
+namespace SenjayVPA
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class SenjayVanillaPlusAddition : BaseUnityPlugin
+    {
+        private const string Author = "Senjay";
+        private const string Name = "Senjay's Vanilla Plus Addition";
+        private const string Version = "1.0.0.0";
+        private const string GUID = Author + ".VanillaPlusAddition";
+
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        private static SenjayVanillaPlusAddition Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            harmony.PatchAll(typeof(SenjayVanillaPlusAddition));
+            harmony.PatchAll(typeof(SkipInitSelectionClass));
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(GUID);
+
+            mls.LogInfo(Name + " has been loaded into the game");
+
+            //Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
